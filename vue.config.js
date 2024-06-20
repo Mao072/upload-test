909dfce9de8567d445c3f7180ddf8a65c0ba0461
ryan <ryanwu0517@gmail.com>
2024-06-20 20:10:27 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: '/upload-test/',
+  // 其他有效的配置项
+  outputDir: 'dist',
+  assetsDir: 'static',
+  productionSourceMap: false,
+  devServer: {
+    port: 8080,
+    open: true,
+  },
+};
